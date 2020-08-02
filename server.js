8172ac12bf2afeb1a5c7e3707a40560998d1d85a
Leonardod <lfdantoni@gmail.com>
2020-08-02 17:30:35 -0300

    fixed server port

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ server.use((req, res, next) => {
 
 // Use default router
 server.use(router)
-server.listen(3010, () => {
+server.listen(process.env.PORT || 3010, () => {
   console.log('JSON Server is running')
 });
